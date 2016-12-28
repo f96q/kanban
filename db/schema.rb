9ef1dce77ba37c92a1fe7710db16f79f15c3cb88
danny <danny.dev8@gmail.com>
2016-12-28 18:33:03 +0900

    Add board

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161228092645) do
+ActiveRecord::Schema.define(version: 20161228093144) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
