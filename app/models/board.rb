1f8f819b11e8ad57676b96caaf093666e6c2aa9c
danny <danny.dev8@gmail.com>
2016-12-28 19:19:56 +0900

    Add association

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,4 @@
 class Board < ApplicationRecord
+  has_many :columns
+  has_many :user_boards
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   acts_as_list scope: :column_id
+
+  belongs_to :column
 end
